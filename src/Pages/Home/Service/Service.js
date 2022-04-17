8c736dc80f0b6a8385d51a41ef926771df5da7d6
Sadia-90 <sadiamahmood24@gmail.com>
2022-04-17 12:12:41 +0600

    set banner

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './Service.css'
 
 const Service = ({service}) => {
 
     const {img, name, price, discription} = service;
     return (
-        <div>
-            <img src={img} alt="" />
+        <div className='service'>
+            <img width={350} height={350} src={img} alt="" />
             <h2>Name: {name}</h2>
             <h1>Price: {price}</h1>
             <p>Discription: {discription}</p>
-            <button>Booking: {name}</button>
+            <button className='button'>Booking: {name}</button>
         </div>
     );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
+import './Services.css'
 
 
 const Services = () => {
@@ -13,14 +14,16 @@ const Services = () => {
 
     return (
         <div>
-            <h2>Services</h2>
+            <h2 className='service-title'>Services</h2>
 
-            {
+           <div className='services-container'>
+           {
               services.map(service => <Service
                 key={service.id}
                 service={service}
               ></Service>)  
             }
+           </div>
         </div>
     );
 };
