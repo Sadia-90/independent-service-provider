eeb8580c6c48522fe973e1645ec4bf3a6c9002b1
Sadia-90 <sadiamahmood24@gmail.com>
2022-04-18 11:16:40 +0600

    blog section

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h2>this is myself</h2>
+        <div className='goal-container'>
+            <div>
+                 <img src="https://photos.google.com/photo/AF1QipOuxz8E1DdPMyxyH2ecNfn1GQ6V2D9wDDXIkFDi" alt="" />
+            </div>
+            <h1>It has already been six month past I started of 2021!</h1>
+            <h3>My goal below there </h3>
+            <li>1.I want to become a web devoloper.</li>   
+            <li> 2. Now i learn javascript, react HTML,CSS.</li>
+            <li> 3. As a beginner firstly i want to get a internship.</li>
+            <li> 4. After that work I want to get a job.</li>
+            <li>5.I believe as soon as perfect time i become a web devoloper.</li>
+           
+            
+
         </div>
     );
 };
--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Blog.css'
 
 const Blog = () => {
     return (
-        <div>
-            <h1 className=''>Authorization vs authentication</h1>
+        <div className='blog-container'>
+            <h3>Authorization vs authentication</h3>
             <p>1.authentication is the process of verifying who someone is.
             authorization is the process of verifying what specific applications, files, and data a user has access.
               <br/>
@@ -11,14 +12,14 @@ const Blog = () => {
             </p>
 
 
-            <h1>Why are i using firebase? What other options do i have to implement authentication?</h1>
+            <h3>Why are i using firebase? What other options do i have to implement authentication?</h3>
 
 
-            <h2>Firebase manages all data real-time in the database</h2>
+            <h4>Firebase manages all data real-time in the database</h4>
 
             <p>Authentication makes Web page secure sign-ins for any platform. This end-to-end solution supports email and passowrd, phone auth, & mult-platform login. Monitor App Performance. Customize our App. Cross-Platform Solutions. Boost App Engagement</p>
 
-            <h1> What other services does firebase provide other than authentication</h1>
+            <h3> What other services does firebase provide other than authentication</h3>
             <p>Open Source Backend Platform.Firebase to help take our mobile app development a notch higher.Simple core library, with a modular API for our application.</p>
             
         </div>
